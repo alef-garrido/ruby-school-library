48c5d1db37a8b319d884923144dec42f84b87242
alef-garrido <oa.g11@icloud.com>
2022-05-25 18:12:33 -0500

    Add Person class file

---
+++ Person.rb
@@ -0,0 +1,14 @@
+class Person
+  attr_accessor :name :age
+  attr_reader :id
+  
+  def initialize(name = "Unknown", age, parent_permission = true)
+    @id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+ 
+
+end
