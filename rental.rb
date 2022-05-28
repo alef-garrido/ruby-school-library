87a858c681d365db5ac1bf938cc0d350b3657aab
alef-garrido <oa.g11@icloud.com>
2022-05-28 03:48:47 -0500

    Refactor class rental

--- rental.rb
+++ rental.rb
@@ -1,7 +1,11 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :person, :book
 
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+    @person = person
+    person.rentals << self unless person.rentals.include?(self)
+    @book = book
+    book.rentals << self unless book.rentals.include?(self)
   end
 end
