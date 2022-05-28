b4a9737ca379f89ee033624fb28e9a17651107de
alef-garrido <oa.g11@icloud.com>
2022-05-28 04:34:48 -0500

    add list-people method

--- app.rb
+++ app.rb
@@ -12,6 +12,20 @@ class App
     @rentals = []
   end
 
+  def list_all_books
+    puts "\n"
+    @books.each_with_index do |book, index|
+      puts "#{index + 1} Title: #{book.title}, Author: #{book.author}"
+    end
+  end
+
+  def list_all_people
+    puts "\n"
+    @people.each_with_index do |person, index|
+      puts "#{index + 1} [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
+
   def option_run(input)
     case input
     when 1
@@ -28,13 +42,6 @@ class App
       list_rentals_by_id
     else
       puts 'Wrong value. Verify selection'
-    end
-  end
-
-  def list_all_books
-    puts "\n"
-    @books.each_with_index do |book, index|
-      puts "#{index + 1} Title: #{book.title}, Author: #{book.author}"
     end
   end
 
