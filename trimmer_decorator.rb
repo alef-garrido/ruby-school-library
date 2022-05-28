48109ac71992868853afaeb5eb631ec5dea88ace
alef-garrido <oa.g11@icloud.com>
2022-05-28 02:47:44 -0500

    Add classroom class

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,10 @@ require_relative 'base_decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name.slice(0, 10) if @nameable.correct_name.length > 10
+    if @nameable.correct_name.length > 10
+      @nameable.correct_name.slice(0, 10)
+    else
+      @nameable.correct_name
+    end
   end
 end
