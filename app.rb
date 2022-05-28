75956f50300fcb37a87ecdff05dfac2a804a1e6b
alef-garrido <oa.g11@icloud.com>
2022-05-28 15:31:57 -0500

    refactor method to solve detected rubocop offense

--- app.rb
+++ app.rb
@@ -28,72 +28,70 @@ class App
       puts 'There are no books to show'
       return
     end
-    @books.each_with_index do |book, index|
-      puts "#{index + 1} Title: #{book.title} by Author: #{book.author}"
-    end
+    @books.each { |book| puts "Title: #{book.title} by Author: #{book.author}" }
+  end
 
-    def select_person_from_list
-      puts 'Select a person'
-      @people.each_with_index do |person, index|
-        print "#{index}) #{person.is_a?(Teacher) ? '[Teacher]' : '[Student]'} "
-        puts "Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      end
-      gets.chomp.to_i
+  def select_person_from_list
+    puts 'Select a person'
+    @people.each_with_index do |person, index|
+      print "#{index}) #{person.is_a?(Teacher) ? '[Teacher]' : '[Student]'} "
+      puts "Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
+    gets.chomp.to_i
+  end
 
-    def list_people
-      if @people.empty?
-        puts 'No entries. Add one.'
-        return
-      end
-      @people.each do |person|
-        print '[Teacher] ' if person.is_a?(Teacher)
-        print '[Student] ' if person.is_a?(Student)
-        puts "Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      end
+  def list_people
+    if @people.empty?
+      puts 'No entries. Add one.'
+      return
     end
-
-    def add_teacher(age, name, specialization)
-      @people.push(Teacher.new(age, specialization, name))
+    @people.each do |person|
+      print '[Teacher] ' if person.is_a?(Teacher)
+      print '[Student] ' if person.is_a?(Student)
+      puts "Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
+  end
 
-    def add_student(age, name, permission)
-      @people.push(Student.new(age, @classroom, name, parent_permission: permission))
+  def add_teacher(age, name, specialization)
+    @people.push(Teacher.new(age, specialization, name))
+  end
+
+  def add_student(age, name, permission)
+    @people.push(Student.new(age, @classroom, name, parent_permission: permission))
+  end
+
+  def add_book(title, author)
+    @books.push(Book.new(title, author))
+  end
+
+  def can_create_rental?
+    if @books.empty?
+      puts 'No items to show, add a rental.'
+      return false
     end
-
-    def add_book(title, author)
-      @books.push(Book.new(title, author))
+    if @people.empty?
+      puts 'No names'
+      return false
     end
+    true
+  end
 
-    def can_create_rental?
-      if @books.empty?
-        puts 'No items to show, add a rental.'
-        return false
-      end
-      if @people.empty?
-        puts 'No names'
-        return false
-      end
-      true
+  def add_rental(date, index_person, index_book)
+    @rentals.push(Rental.new(date, @people[index_person], @books[index_book]))
+  end
+
+  def list_rentals_by_id
+    if @rentals.empty?
+      puts 'No Rentals to show'
+      return
     end
-
-    def add_rental(date, index_person, index_book)
-      @rentals.push(Rental.new(date, @people[index_person], @books[index_book]))
-    end
-
-    def list_rentals_by_id
-      if @rentals.empty?
-        puts 'No Rentals to show'
-        return
-      end
-      print 'ID of person: '
-      id = gets.chomp
-      puts 'Rentals'
-      @rentals.each do |rental|
-        if rental.person.id == id.to_i
-          print("Date: #{rental.date} ")
-          puts("Book \"#{rental.book.title}\" by #{rental.book.author} ")
-        end
+    print 'ID of person: '
+    id = gets.chomp
+    puts 'Rentals'
+    @rentals.each do |rental|
+      if rental.person.id == id.to_i
+        print("Date: #{rental.date} ")
+        puts("Book \"#{rental.book.title}\" by #{rental.book.author} ")
       end
     end
   end
